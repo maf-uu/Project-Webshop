635ba4ecd9adb0687a85b36ec6b9af5cd13d7204
maf-uu <borsandras2007@gmail.com>
2026-02-23 23:08:03 +0100

    Add files via upload

---
+++ src/routes/itemroutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const multer = require("multer");
+const { uploadItem } = require('./itemcontrol.js');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+
+const router = express.Router();
+
+router.get("/main", (req, res) => {
+    res.json({httpMethod: "get"});
+});
+router.post("/main", (req, res) => {
+    res.json({httpMethod: "post"});
+});
+router.put("/main", (req, res) => {
+    res.json({httpMethod: "put"});
+});
+router.delete("/main", (req, res) => {
+    res.json({httpMethod: "delete"});
+});
+
+router.post("/upload", upload.single('file'), uploadItem);
+
+module.exports = router;
