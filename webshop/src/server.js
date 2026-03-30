2320fca066d1b78d3c00fc959bbe964dac2be802
maf-uu <borsandras2007@gmail.com>
2026-03-30 08:21:05 +0200

    Use env instead
    Updated SMTP credentials to use environment variables for security.

--- webshop/src/server.js
+++ webshop/src/server.js
@@ -12,8 +12,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false, // Use true for port 465, false for port 587
   auth: {
-    user: "eisen.shop.reply@gmail.com",
-    pass: "jn7jnAPss4f63QBp6D",
+    user: SMTP_USER,
+    pass: STMP_PASS,
   },
 });
 
